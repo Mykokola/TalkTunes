c50657b031af62871ba79eabca3cce6f247b58dc
Mykokola <119622278+Mykokola@users.noreply.github.com>
2023-11-29 18:28:34 -0600

    Update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -7,24 +7,5 @@ export async function GET(request: Request){
         });
     } catch (error) {
         console.error("Помилка при виконанні запиту до бази даних:", error);
-        
-        // Відправка помилки клієнту
     }
 }
-// export default async function Cart({
-//   params
-// } : {
-//   params: { user: string }
-// }): Promise<JSX.Element> {
-//   const data= await sql`SELECT id,type FROM car`;
-//   const { rows } = data
-//   return (
-//     <div>
-//       {rows.map((row) => (
-//         <div key={row.id}>
-//           {row.type}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
